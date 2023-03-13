89e1fe42f46f71d233cb7950774dc19b7df30ccf
Berkantagur <berkantagur6@gmail.com>
2023-03-13 13:42:29 +0300

    .py

---
+++ Alphabet.py
@@ -0,0 +1,11 @@
+
+vowel = ['a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü']
+silent = ['b', 'c', 'ç', 'd', 'f', 'g', 'ğ', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 'ş', 't', 'v', 'y', 'z']
+
+letter = input("Enter a letter: ")
+
+if letter in vowel:
+    print(letter, "is a vowel letter.")
+
+else:
+    print(letter, "is a silent letter.")
