9e16c8344c9de7d6a49365292f2c062fac82ec4f
Berkantagur <berkantagur6@gmail.com>
2023-03-13 12:56:02 +0300

    .py

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+
+operation = input("Please select the operation you want to perform (+ - / *): ")
+
+if operation == '+' or operation == '-' or operation == '/' or operation == '*' :
+        
+    number1 = input("Please enter the first integer number: ")
+    number2 = input("Please enter the second integer number: ")
+
+    if operation == '+':
+        result = int(number1) + int(number2)
+
+    elif operation == '-':
+        result = int(number1) - int(number2)
+
+    elif operation == '/':
+        result = int(number1) / int(number2)
+
+    elif operation == '*':
+        result = int(number1) * int(number2)
+
+    print("Result: ", result)
+
+else :
+    print("Invalid operation! Please select valid operation (+ - / *)...")
