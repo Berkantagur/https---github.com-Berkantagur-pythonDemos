0902eaefb3f090a3d09f45fba28c392b3aea2f2e
Berkantagur <berkantagur6@gmail.com>
2023-03-13 13:57:40 +0300

    .py

---
+++ usernamePassword.py
@@ -0,0 +1,9 @@
+
+username = input("Please enter the username: ")
+password = input("Please enter the password: ")
+
+if username == 'admin' and password == 'password':
+    print("The entry was successful...")
+
+else: 
+    print("The entry failed!!!")
