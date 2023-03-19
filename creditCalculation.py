82a1142557adcbdd62cff51ee0cc3e48b5ff120b
Berkantagur <berkantagur6@gmail.com>
2023-03-19 14:44:51 +0300

    .py

--- creditCalculation.py
+++ creditCalculation.py
@@ -9,34 +9,7 @@ print(type(money))
 print(type(creditName))
 
 rate = money * 0.10 * (int(term)/12)
-print("Rate:" + str(rate))
+print("Rate:" + str(rate) + "TL")
 
 totalPayment = rate + money
-print("Total Money:" + str(totalPayment))
-
-
-
-
-
-
-
-
-
-
-term = input("Please enter the number of terms that you want: ")
-print(term)
-print(type(rate))
-print(int(term) + 12)
-
-#string interpolation
-print("The term that arises as a result of your choice: " + term)
-print("The term that arises as a result of your choice:  {newTerm}".format(newTerm = term))
-print(f"The term that arises as a result of your choice: {term}")
-
-name = "Berkant"
-text = "Hello {names}".format(names = name)
-print(text)
-
-#f-string
-text = f"Welcome {name}"
-print(text)
+print("Total Money:" + str(totalPayment) + "TL")
