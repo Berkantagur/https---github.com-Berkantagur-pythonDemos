d2b051d2280f4b13f7fcfcd1258ed4c0c97eb4e1
Berkantagur <berkantagur6@gmail.com>
2023-03-19 14:18:07 +0300

    .py

--- creditCalculation.py
+++ creditCalculation.py
@@ -10,3 +10,7 @@ print(int(term) + 12)
 #print(str(creditName))
 rate = str(rate)
 print(type(rate))
+
+term = input("Please enter the number of terms that you want: ")
+print(term)
+print(type(rate))
