260a544a570946229104aa44b6869e36ff654be0
Berkantagur <berkantagur6@gmail.com>
2023-03-20 14:37:16 +0300

    .py

---
+++ creditCalculation1.py
@@ -0,0 +1,17 @@
+
+creditRate = input("Please enter the interest rate: ")
+term = input("Please enter the number of terms that you want: ")
+money = input("Please enter the principal amount: ")
+
+creditRate = float(creditRate)
+term = int(term)
+money = int(money)
+
+rate = money * 0.10 * (term/12)
+print("Rate:" + str(rate) + "TL")
+
+totalPayment = rate + money
+
+print("Total Money: {totalPayment} TL".format(totalPayment = totalPayment))
+print(f"Total Money: {totalPayment} TL")
+#string interpolation
