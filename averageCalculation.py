d8929972c047965cd117ebe47bab24d574ca1fc1
Berkantagur <berkantagur6@gmail.com>
2023-03-21 20:06:37 +0300

    .py

--- averageCalculation.py
+++ averageCalculation.py
@@ -1,8 +1,12 @@
+
 vize = int(input("Please enter your vize mark score:"))
 final = int(input("Please enter your final mark score:"))
 
 average = vize * 0.40 + final  * 0.60
-print(average)
+print("Your mark average:", average)
 
 if average <= 30:
     print("You are failed!")
+
+else:
+    print("Congratulations, You've passed...")
