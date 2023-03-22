1b42274726aaee5bdacc35ee9590cb21865bd087
Berkantagur <berkantagur6@gmail.com>
2023-03-22 12:24:19 +0300

    .py

---
+++ distance.py
@@ -0,0 +1,6 @@
+x1 = input("Please enter the x-coordinate of the first point:")
+y1 = input("Please enter the y-coordinate of the first point:")
+
+x2 = input("Please enter the x-coordinate of the second point:")
+x2 = input("Please enter the y-coordinate of the second point:")
+
