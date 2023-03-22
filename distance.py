a38e0de5dba2c3f3a2e46e0067f83526090cf698
Berkantagur <berkantagur6@gmail.com>
2023-03-22 12:31:09 +0300

    .py

--- distance.py
+++ distance.py
@@ -1,10 +1,10 @@
 import math
 
-x1 = input("Please enter the x-coordinate of the first point:")
-y1 = input("Please enter the y-coordinate of the first point:")
+x1 = int(input("Please enter the x-coordinate of the first point:"))
+y1 = int(input("Please enter the y-coordinate of the first point:"))
 
-x2 = input("Please enter the x-coordinate of the second point:")
-y2 = input("Please enter the y-coordinate of the second point:")
+x2 = int(input("Please enter the x-coordinate of the second point:"))
+y2 = int(input("Please enter the y-coordinate of the second point:"))
 
 d1 = pow(x1-x2, 2)
 d2 = pow(y1-y2, 2)
