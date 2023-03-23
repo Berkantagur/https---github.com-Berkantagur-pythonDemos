23c806d43dc26287fbec067e06769cf667a66327
Berkantagur <berkantagur6@gmail.com>
2023-03-23 11:50:31 +0300

    .py

--- humanAction.py
+++ humanAction.py
@@ -1,5 +1,15 @@
 
 class Human:
 
-    def talk(self, sentence):
+    #self => this
+    def greeting(self, sentence):
         print(f"{self.name}: {sentence}")
+
+    def walk(self):
+        print(f"{self.name} is walking.")
+
+#instance => örnek
+human1 = Human()
+human1.name = "Enes"
+human1.greeting("Hello", "Berkant")
+human1.walk()
