7eaa68c6f10619b4cec034394b2e38038e92ffa8
Berkantagur <berkantagur6@gmail.com>
2023-03-23 14:45:03 +0300

    .py

--- mathematics.py
+++ mathematics.py
@@ -9,3 +9,13 @@ def division(n1, n2):
     else:
         return n2/n1
     
+def multiply(n1, n2):
+    return n1 * n2
+
+def subtraction(n1, n2):
+    
+    if n1>=n2:
+        return n1-n2
+    
+    else:
+        return n2-n1
