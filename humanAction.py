fbd1373b170c6153dec429116556f82b8c6b1ba1
Berkantagur <berkantagur6@gmail.com>
2023-03-23 12:07:55 +0300

    .py

--- humanAction.py
+++ humanAction.py
@@ -3,18 +3,18 @@ class Human:
 
     #self => this
     sentence = "Hello"
+    name = "Ali"
     def greeting(self):
         print(f"{self.name}: {self.sentence}")
 
-    def walk(self):
-        print(f"{self.name} is walking.")
+    def greeting1(self):
+        print(f"{self.name}: {self.sentence1}")
 
 #instance => örnek
 human1 = Human()
-human1.name = "Berkanr"
-human1.greeting("Berkant")
-human1.walk()
+human1.name = "Berkant"
+human1.greeting()
 
 human2 = Human()
-human2.name = "Ahmet"
-human2.greeting("Hi", "Kaan")
+human2.sentence1 = "Hi"
+human2.greeting1()
