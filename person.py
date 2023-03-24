2d1d6905d50f55f84accfa7aefbafd332f2474e3
Berkantagur <berkantagur6@gmail.com>
2023-03-24 08:49:36 +0300

    .py

--- person.py
+++ person.py
@@ -1,12 +1,12 @@
-class person ():
+class Person():
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
 
 
-customer1 = person("Engin","Demiroğ")
-customer2 = person("Kerem","Varış")
-customer3 = person("Berkant","Ağur")
+customer1 = Person("Engin","Demiroğ")
+customer2 = Person("Kerem","Varış")
+customer3 = Person("Berkant","Ağur")
 
 print(customer1.name + " " + customer1.surname)
 print(customer2.name + " " + customer2.surname)
