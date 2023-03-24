2b93e16de0d911fb195149385922c9d86dd4b397
Berkantagur <berkantagur6@gmail.com>
2023-03-24 09:50:18 +0300

    .py

--- miniProject/project.py
+++ miniProject/project.py
@@ -27,3 +27,16 @@ def removeStudent():
     print(students)
 
 # removeStudent()
+
+def addMultiStudent():
+    
+    print(colored("\t~~~~~~ADDING MORE THAN ONE STUDENT TO THE STUDENT LIST~~~~~~", "light_red"))
+
+    stName = input("Please enter the name of the student to be added to the students list: ")
+    stSurname = input("Please enter the surname of the student to be added to the students list: ")
+
+    students.append(stName + " " + stSurname)
+    print(colored(f"{stName} {stSurname} has been added to the students list...", "light_green"))
+    print(students)
+
+addMultiStudent()
