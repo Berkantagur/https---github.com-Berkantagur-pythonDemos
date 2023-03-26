883a368eb1a47117af5f72115d7bfdf093e4c341
Berkantagur <berkantagur6@gmail.com>
2023-03-26 12:39:17 +0300

    .py

--- seleniumExample.py
+++ seleniumExample.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.maximize_window()
 driver.get("https://www.google.com/")
+input = driver.find_element(By.NAME, "q")
+print(f"Input: {input}")
+
 # sleep(10)
 while True:
     continue
