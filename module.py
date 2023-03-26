691ab567e48814a8bc6e9799b194e3345126c4eb
Berkantagur <berkantagur6@gmail.com>
2023-03-26 11:26:28 +0300

    .py

--- module.py
+++ module.py
@@ -1,10 +1,9 @@
 import mathematics as math
+import random
 
 print(math.sum(10,20))
 print(math.division(10,20))
 print(math.multiply(10,20))
 print(math.subtraction(10,20))
-
-import random
 
 print(random.randint(0,100))
