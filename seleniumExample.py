9abf62d11e4759b293947a1262fc9478f8e5f5d8
Berkantagur <berkantagur6@gmail.com>
2023-03-26 11:43:14 +0300

    .py

--- seleniumExample.py
+++ seleniumExample.py
@@ -3,4 +3,5 @@ from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://www.google.com/")
 sleep(20)
